a4e544ed1a5ebcb90bdeaf2d0ff3d4e290fc774f
Johann Woelper <woelper@gmail.com>
2020-09-07 23:31:52 +0200

    add largest file feature

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,9 @@ use std::collections::HashMap;
 use std::path::{Path, PathBuf};
 use jwalk::{WalkDir};
 use rayon::prelude::*;
-
+#[derive(Debug, Clone)]
 pub struct File {
-    size: u32,
+    size: u64,
     ext: Option<String>,
     path: PathBuf,
 }
@@ -16,19 +16,27 @@ pub struct File {
 /// DirInfo holds all info about a diretory.
 pub struct DirInfo {
     pub filetype_sizes: HashMap<String, u64>,
+    pub files: Vec<File>
 }
 
 impl DirInfo {
     pub fn new<P: AsRef<Path>>(source: P) -> DirInfo {
         DirInfo {
             filetype_sizes: HashMap::new(),
+            files: vec![]
         }
     }
 
-    pub fn largest_types(&self) -> Vec<(&String, &u64)> {
+    pub fn types_by_size(&self) -> Vec<(&String, &u64)> {
         let mut count: Vec<_> = self.filetype_sizes.iter().collect();
         count.sort_by(|a, b| b.1.cmp(a.1));
+        count
+        // self.filetype_sizes.iter().cloned().map(|x| x).collect()
+    }
 
+    pub fn files_by_size(&self) -> Vec<File> {
+        let mut count = self.files.clone();
+        count.sort_by(|a, b| b.size.cmp(&a.size));
         count
         // self.filetype_sizes.iter().cloned().map(|x| x).collect()
     }
@@ -45,8 +53,15 @@ pub fn scan<P: AsRef<Path>>(source: P) -> DirInfo {
         // println!("{:?}", x.path());
         if let Some(ext) = x.path().extension() {
             if let Ok(meta) = x.path().metadata() {
+                let size = meta.len();
+                let ext_string = ext.to_string_lossy().to_string(); 
+                *i.filetype_sizes.entry(ext_string.clone()).or_insert(size) += size;
+                i.files.push(File {
+                    size: size,
+                    ext: Some(ext_string),
+                    path: x.path().to_path_buf()
 
-                *i.filetype_sizes.entry(ext.to_string_lossy().to_string()).or_insert(meta.len()) += meta.len() ;
+                })
             }
         }
           
--- src/tests.rs
+++ src/tests.rs
@@ -3,5 +3,6 @@ use super::*;
 #[test]
 fn it_works() {
     let i = scan("/home/woelper/Downloads");
-    dbg!(&i.largest_types()[..10]);
+    dbg!(&i.types_by_size()[..10]);
+    dbg!(&i.files_by_size()[..10]);
 }
