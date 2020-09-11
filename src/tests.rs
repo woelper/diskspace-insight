53cc11bd66f7379851bcba738a0832b7be1e06c2
Johann Woelper <woelper@gmail.com>
2020-09-11 20:28:57 +0200

    update test

--- src/tests.rs
+++ src/tests.rs
@@ -2,7 +2,7 @@ use super::*;
 
 #[test]
 fn it_works() {
-    let i = scan("/home/woelper/Downloads");
-    dbg!(&i.types_by_size()[..10]);
-    dbg!(&i.files_by_size()[..10]);
+    let i = scan(".");
+    dbg!(&i.types_by_size()[..1]);
+    dbg!(&i.files_by_size()[..1]);
 }
